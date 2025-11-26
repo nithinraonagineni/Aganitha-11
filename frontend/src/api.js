f4a93e07fc3aa2727951e48c652ba0730b415568
nithinraonagineni <nagineninithinrao@gmail.com>
2025-11-26 13:25:54 +0530

    Update api.js comitted

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,4 @@
-const API_BASE = import.meta.env.VITE_API_BASE || "http://localhost:4000";
-  const API_BASE = import.meta.env.VITE_API_BASE;
+const API_BASE = import.meta.env.VITE_API_BASE;
 
 
 export async function listLinks() {
